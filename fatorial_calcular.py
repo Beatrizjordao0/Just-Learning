f4e68baae068ebb83d102b4c9bfe79355a120129
bibiperigosa <beatrizjordao15@gmail.com>
2025-03-06 14:10:02 -0300

    First Commit

---
+++ fatorial_calcular.py
@@ -0,0 +1,17 @@
+# Criar uma forma de calcular a função binomial.
+# n, k = n!/(k! * (n - k)!)
+
+
+def fatorial(n):
+    fat = 1
+    while n > 1:
+        fat *= n
+        n -= 1
+    return fat
+
+def funcaoBinomial(n , k):
+    return fatorial(n) / (fatorial(k) * fatorial(n - k))
+
+print(funcaoBinomial(20, 10))
+
+    
